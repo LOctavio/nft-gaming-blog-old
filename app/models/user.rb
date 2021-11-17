fd850c04ce6695f09f6069397af93d6cd71eb193
LOctavio <luis_ram1997@hotmail.com>
2021-11-17 12:08:57 -0600

    Fix most_recent_posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def most_recent_posts(user_id)
-    User.find_by(id: user_id).posts.order(created_at: :desc).limit(3)
+  def most_recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
 end
