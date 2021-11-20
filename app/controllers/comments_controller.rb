0172bc3931e819c40f1993083a237b72de5e8277
LOctavio <luis_ram1997@hotmail.com>
2021-11-20 10:18:16 -0600

    Fix typo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
-    @comments = @post.comments.build
+    @comment = @post.comments.build
   end
 
   def create
