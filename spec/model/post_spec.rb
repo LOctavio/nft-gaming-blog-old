3efd21de0148820448ca2f4fd0518697c7db9c86
LOctavio <luis_ram1997@hotmail.com>
2021-11-20 16:16:28 -0600

    Add validation tests

---
+++ spec/model/post_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  subject {Post.new(title: 'New post', text: 'this is a new post', comments_counter: 0, likes_counter: 0, author_id: 1)}
+
+  before {
+    user = User.create(id: 1, name: 'Luis', photo: 'photo-url', bio: 'bio', post_counter: 0)
+    subject.save
+  }
+
+  it 'Title must not be blank' do
+    subject.title = ''
+    expect(subject).to_not be_valid
+  end
+
+  it 'Title must not exceed 250 characters.' do
+    subject.title = 'title'
+    expect(subject).to be_valid
+  end
+
+  it 'CommentsCounter must be an integer greater than or equal to zero.' do
+    subject.comments_counter = 0
+    expect(subject).to be_valid
+  end
+
+  it 'LikesCounter must be an integer greater than or equal to zero.' do
+    subject.likes_counter = 0
+    expect(subject).to be_valid
+  end
+end
---
+++ spec/model/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject {User.new(name: 'Luis', photo: 'photo-url', bio: 'bio', post_counter: 0)}
+
+  before {subject.save}
+
+  it 'Name must not be blank' do
+    subject.name = ''
+    expect(subject).to_not be_valid
+  end
+
+  it 'PostsCounter must be an integer greater than or equal to zero.' do
+    subject.post_counter = 0
+    expect(subject).to be_valid
+  end
+
+  it 'PostsCounter must not be an integer smaller than zero.' do
+    subject.post_counter = -1
+    expect(subject).to_not be_valid
+  end
+end
