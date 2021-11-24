ba2450c05fd8dcbeaed5493c9963d0f72746894c
LOctavio <luis_ram1997@hotmail.com>
2021-11-24 13:28:11 -0600

    Add new form to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @user = User.find(current_user.id)
+    @post = @user.posts.new
+  end
+
+  def create
+    @user = User.find(current_user.id)
+    @post = @user.posts.new(post_params)
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :text, :comments_counter, :likes_counter)
+  end
 end
