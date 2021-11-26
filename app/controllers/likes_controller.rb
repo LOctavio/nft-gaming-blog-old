7ed5a78f76dd6bd45e82760950783aa618e83e1b
LOctavio <luis_ram1997@hotmail.com>
2021-11-26 17:28:36 -0600

    Fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,10 +4,10 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(author_id: current_user.id, post_id: params[:post_id])
     flash[:alert] = if @like.save
-      'You have successfully liked this post'
-    else
-      'This is an invalid like'
-    end
+                      'You have successfully liked this post'
+                    else
+                      'This is an invalid like'
+                    end
     redirect_to user_post_path(User.find(params[:user_id]), @post)
   end
 
