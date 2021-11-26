531a36b312a38e48b9cf53337078ac4eda2713f7
LOctavio <luis_ram1997@hotmail.com>
2021-11-25 20:37:20 -0600

    Add likes qhen click on icon

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,15 @@
+class LikesController < ApplicationController
+  before_action :find_post
+
+  def create
+    @like = Like.new(author_id: current_user.id, post_id: params[:post_id])
+    @like.save
+    redirect_to user_post_path(User.find(params[:user_id]), @post)
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+end
