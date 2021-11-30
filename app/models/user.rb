459e92038bf8fa20ce158af52421179ea36b09c3
LOctavio <luis_ram1997@hotmail.com>
2021-11-30 15:12:54 -0600

    Allow users to register an account and log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
+  validates :email, confirmation: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    get "/logout" => "devise/sessions#destroy"
+  end                                                                            
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create] do
@@ -8,4 +12,5 @@ Rails.application.routes.draw do
     end
   end
   get '/new_post', to: 'posts#new'
+  get '/login', to: 'devise/sessions#new'
 end
